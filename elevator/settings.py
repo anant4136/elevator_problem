6a4abc77a318502a5169e18e0a7ee00390109249
anant4136 <anant4136@gmail.com>
2023-06-02 14:53:12 +0530

    using sqlite3

--- elevator/settings.py
+++ elevator/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'elevator_application',
+    'elevator_application'
 ]
 
 MIDDLEWARE = [
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'elevator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'anant4136',
-        'PASSWORD': 'ANant@4136',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'anant4136',
+#         'PASSWORD': 'ANant@4136',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
